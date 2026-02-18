cc5a4fc75fcfeb185390597c7d22720bec0bc53f
jhon james <und_6889@hotmail.com>
2026-02-17 21:55:03 -0500

    firts commit up

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+  // Cambiar icono ☰ -> ✕
+  if(menu.classList.contains("active")){
+    toggle.textContent = "✕";
+  } else {
+    toggle.textContent = "☰";
+  }
+});
